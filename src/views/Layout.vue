ef7c19bb86af7af095ef214c1527b7b13aa3850b
jhao96 <1150307149@qq.com>
2019-12-02 11:16:15 +0800

    项目结构初始

---
+++ src/views/Layout.vue
@@ -0,0 +1,18 @@
+
+<template>
+<!-- 公共布局 -->
+  <div>
+      <!-- 头部 -->
+      <!-- 内容容器 -->
+      <!-- tao栏 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
